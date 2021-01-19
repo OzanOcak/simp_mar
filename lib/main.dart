b0cd9ffba460d35fa24f616df9a676e49c21414b
ozan <ozan@ozans-MacBook-Pro.local>
2021-01-19 11:39:15 -0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:mar/mario_animation_demo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'simp mario',
+      home: MarioAnimationDemo(),
+    );
+  }
+}
---
+++ lib/mario_animation_demo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MarioAnimationDemo extends StatefulWidget {
+  @override
+  _MarioAnimationDemoState createState() => _MarioAnimationDemoState();
+}
+
+class _MarioAnimationDemoState extends State<MarioAnimationDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
