3a234b8fd0acd6c67019ea2157ba5ec1b6f04aba
ozan <ozan@ozans-MacBook-Pro.local>
2021-01-19 15:17:41 -0500

    move y frame and add weight

--- lib/mario_animation_demo.dart
+++ lib/mario_animation_demo.dart
@@ -24,12 +24,28 @@ class _MarioAnimationDemoState extends State<MarioAnimationDemo>
     animationController =
         AnimationController(vsync: this, duration: Duration(seconds: 8));
 
+    List<double> weights = [1.0, 1.0, .3, .4, 1.0];
+
     marioX = TweenSequence<double>([
-      TweenSequenceItem(tween: Tween(begin: 0.0, end: 0.5), weight: 1),
-      TweenSequenceItem(tween: Tween(begin: 0.5, end: 0.5), weight: 1),
-      TweenSequenceItem(tween: Tween(begin: 0.5, end: 0.5), weight: 1),
-      TweenSequenceItem(tween: Tween(begin: 0.5, end: 0.5), weight: 1),
-      TweenSequenceItem(tween: Tween(begin: 0.5, end: 1), weight: 1),
+      TweenSequenceItem(tween: Tween(begin: 0.0, end: 0.5), weight: weights[0]),
+      TweenSequenceItem(tween: Tween(begin: 0.5, end: 0.5), weight: weights[1]),
+      TweenSequenceItem(tween: Tween(begin: 0.5, end: 0.5), weight: weights[2]),
+      TweenSequenceItem(tween: Tween(begin: 0.5, end: 0.5), weight: weights[3]),
+      TweenSequenceItem(tween: Tween(begin: 0.5, end: 1), weight: weights[4]),
+    ]).animate(animationController);
+
+    marioY = TweenSequence<double>([
+      TweenSequenceItem(tween: Tween(begin: 0.0, end: 0.0), weight: weights[1]),
+      TweenSequenceItem(tween: Tween(begin: 0.0, end: 0.0), weight: weights[2]),
+      TweenSequenceItem(
+          tween: Tween(begin: 0.0, end: 1.0)
+              .chain(CurveTween(curve: Curves.easeOutQuad)),
+          weight: weights[3]),
+      TweenSequenceItem(
+          tween: Tween(begin: 1.0, end: 0.0)
+              .chain(CurveTween(curve: Curves.easeIn)),
+          weight: weights[4]),
+      TweenSequenceItem(tween: Tween(begin: 0.0, end: 0.0), weight: weights[0]),
     ]).animate(animationController);
 
     animationController.repeat();
@@ -47,7 +63,7 @@ class _MarioAnimationDemoState extends State<MarioAnimationDemo>
               builder: (context, child) {
                 return Positioned(
                   left: marioX.value * size.width - 16,
-                  top: 200,
+                  top: size.height / 2 - 80 * marioY.value,
                   child: Image(image: AssetImage('assets/mario_1.png')),
                 );
               })
